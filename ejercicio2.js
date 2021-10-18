7bc2e7445ace63993b68bf982570b4629d56bbe5
Agustin Mebar <agustin415@gmail.com>
2021-10-18 20:42:29 -0300

    primer

---
+++ ejercicio2.js
@@ -0,0 +1,20 @@
+// A- Mayor de edad
+var n1 = 18 //edad
+
+if (n1 >= 18) {
+    console.log("es mayor de edad")
+} else {
+    console.log("es menor de edad")
+}
+
+// B- Suma
+
+var a = 10
+var b = 20
+
+const suma = a + b
+if (suma > a) {
+    console.log("la suma es mayor a la constante A")
+}else if (suma > b) {
+    console.log("la suma es mayor a la constante B")
+}
